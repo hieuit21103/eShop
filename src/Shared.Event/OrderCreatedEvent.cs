7134d7b46a6aa8da0bdaf244f1e502ba2d194ef7
Lê Minh Hiếu <hieuit21103@gmail.com>
2025-07-18 05:58:16 +0700

    Shared.Event Initial Commit

---
+++ src/Shared.Event/OrderCreatedEvent.cs
@@ -0,0 +1,10 @@
+﻿namespace Shared.Event;
+
+public class OrderCreatedEvent : IntegrationEvent
+{
+    public Guid OrderId { get; set; }
+    public string UserId { get; set; }
+    public string Status { get; set; }
+    public decimal TotalPrice { get; set; }
+    public List<OrderItem> Items { get; set; }
+}
